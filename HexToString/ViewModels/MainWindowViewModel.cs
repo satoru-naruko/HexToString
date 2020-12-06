1a39836bec703507997efb850cf1fff4fe7a92d4
satoru-naruko <naruko505@gmail.com>
2020-12-06 18:57:00 +0900

    ClipBoardへのコピー機能を実装 コード整理

--- HexToString/ViewModels/MainWindowViewModel.cs
+++ HexToString/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,8 @@
 using Prism.Mvvm;
 using System;
 using System.IO;
+using System.Text;
+using System.Windows;
 
 namespace HexToString.ViewModels
 {
@@ -21,35 +23,64 @@ namespace HexToString.ViewModels
             set { SetProperty(ref _filePath, value); }
         }
 
+        private string _outHexStr;
+        public string OutHexStr
+        {
+            get { return _outHexStr; }
+            set { SetProperty(ref _outHexStr, value); }
+        }
+
         public DelegateCommand ExecuteCmd { get; }
+        public DelegateCommand CopyToClipboardCmd { get; }
 
         public MainWindowViewModel()
         {
             ExecuteCmd = new DelegateCommand(ExecuteHexToStr);
+            CopyToClipboardCmd = new DelegateCommand(CopyToClipboardExecute);
         }
 
         public void ExecuteHexToStr() 
         {
             string path = FilePath;
+            Encoding enc = Encoding.GetEncoding("utf-8");
 
             using (FileStream fs = new FileStream(path, FileMode.Open))
             using (BinaryReader br = new BinaryReader(fs))
+            using (var writer = new StreamWriter(@"test.txt", false /*append*/, enc))
             using (var baseStreasm = br.BaseStream)
             {
-                var count = 0;
+                var charCount = 0;
+                var newLine = false;
+                var str = string.Empty;
                 while (baseStreasm.Position != baseStreasm.Length)
                 {
-                    var byteData = br.ReadByte();
-                    Console.Write("0x{0:x2}", byteData);
-                    Console.Write(",");
-                    count++;
-                    if (count == 16)
+                    if (newLine)
                     {
-                        count = 0;
-                        Console.WriteLine("");
+                        str += ", ";
+                        str += "\n";
+                        newLine = false;
+                    }
+
+                    var byteData = br.ReadByte();
+                    str += string.Format(@"0x{0:x2}", byteData);
+                    charCount++;
+                    if (charCount == 16)
+                    {
+                        charCount = 0;
+                        newLine = true;
+                    } else
+                    {
+                        str += ", ";
                     }
                 }
+                OutHexStr = str;
+                writer.Write(str);
             }
+        }
+
+        public void CopyToClipboardExecute()
+        {
+            Clipboard.SetText(OutHexStr);
         }
     }
 }
